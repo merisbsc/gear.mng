2a45d0416fc745316f8b53a44e356ffac7a71811
Meris Besic <besicmeris@gmail.com>
2020-04-22 20:08:35 +0200

    TODO:
    -gear ausborgen
    -gear zurückgeben
    -email bei ablauf des rentals
    -design änderung (keine alerts)

--- php/return.php
+++ php/return.php
@@ -3,4 +3,6 @@ $user = $_GET["user"];
 $brand = $_GET["brand"];
 $type = $_GET["type"];
 
-echo $user . ";" . $brand . ";" . $type . " gelöscht";
+
+
+echo $user . ";" . $brand . ";" . $type . " gelöscht!";
